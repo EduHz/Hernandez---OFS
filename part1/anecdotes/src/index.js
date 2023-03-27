3f79bda997bd46708ad9f6510dacb232bca45a49
EduHz <eduhzprogramador@gmail.com>
2023-03-27 17:57:51 -0300

    Update index.js

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
 const AnecdoteOfTheDay = (props) => {
   return (
     <>
       <h2>Anecdote of the day</h2>
       <p>{props.anecdote}</p>
-      <p>Has {props.vote} votes</p>
+      <p>has {props.vote} votes</p>
       <button onClick={props.handleVote}>Vote</button>
       <button onClick={props.handleSelect}>Next anecdote</button>
     </>
@@ -20,7 +29,7 @@ const AnecdoteWithMostVotes = (props) => {
       {props.maxVote ? (
         <div>
           <p>{props.bestAnecdote}</p>
-          <p>Has {props.maxVote} votes</p>
+          <p>has {props.maxVote} votes</p>
         </div>
       ) : (
         <p>No votes given</p>
@@ -28,6 +37,8 @@ const AnecdoteWithMostVotes = (props) => {
     </>
   );
 };
+
+const GenerateRandom = () => Math.floor(Math.random() * (6 - 1 + 1) + 0);
 
 const App = () => {
   const [selected, setSelected] = useState(0);
@@ -37,9 +48,9 @@ const App = () => {
   const bestAnecdote = anecdotes[points.indexOf(maxVote)];
 
   const handleSelect = () => {
-    let anecdoteRandom = generateRandom(anecdotes.length);
+    let anecdoteRandom = GenerateRandom(anecdotes.length);
     while (anecdoteRandom === selected) {
-      anecdoteRandom = generateRandom(anecdotes.length);
+      anecdoteRandom = GenerateRandom(anecdotes.length);
     }
     setSelected(anecdoteRandom);
   };
@@ -62,19 +73,6 @@ const App = () => {
     </div>
   );
 };
-
-function generateRandom(length) {
-  return Math.floor(Math.random() * length);
-}
-
-const anecdotes = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
