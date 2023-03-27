3cade0bb52ab59169f474faec2de86c36c8e31be
EduHz <eduhzprogramador@gmail.com>
2023-03-27 18:05:33 -0300

    update

--- part1/courseInfo/src/index.js
+++ part1/courseInfo/src/index.js
@@ -1,6 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+const Content = ({ parts }) => <Part parts={parts} />;
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Total = ({ parts }) => {
+  let sum = parts.reduce((n, { exercises }) => n + exercises, 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = ({ parts }) => {
+  return parts.map((res) => (
+    <p key={res.key}>
+      {res.part} {res.exercises}
+    </p>
+  ));
+};
+
+const App = () => {
+
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { part: "Fundamentals of React", exercises: 10, key: 1 },
+      { part: "Using props to pass data", exercises: 7, key: 2 },
+      { part: "State of a component", exercises: 14, key: 3 },
+    ],
+  };
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
