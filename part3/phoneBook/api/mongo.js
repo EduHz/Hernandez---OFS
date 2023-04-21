9aad73b9440718fbb23df47de95a46afa092ad97
EduHz <eduhzprogramador@gmail.com>
2023-04-21 18:55:45 -0300

    Update mongo.js

--- part3/phoneBook/api/mongo.js
+++ part3/phoneBook/api/mongo.js
@@ -8,33 +8,34 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2];
-
-const url = `mongodb+srv://eduhz:mora@phonebook.dd3s9qe.mongodb.net/note-app?retryWrites=true&w=majority`;
+const name = process.argv[3];
+const number = process.argv[4];
+const url = `mongodb+srv://eduhz:${password}@phonebook.dd3s9qe.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
 
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Person = mongoose.model("Person", personSchema);
 
-const note = new Note({
-  content: "Probando",
-  date: new Date(),
-  important: true,
-});
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
   });
-  mongoose.connection.close();
-});
-
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
