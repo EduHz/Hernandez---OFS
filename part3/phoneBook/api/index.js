f5ab9961e465376e23800fe866acae0fb18c153c
Edu Hz <eduhzprogramador@gmail.com>
2023-04-28 12:34:46 -0300

    finished homework of mongo

--- part3/phoneBook/api/index.js
+++ part3/phoneBook/api/index.js
@@ -1,50 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
+const Person = require("./models/person");
 
-app.use(cors()); //Para compatabilidad
-app.use(express.static("build")); //Para buildear
-
-// Hardcode de array persons
-let persons = [
-  { id: 1, name: "Arto Hellas", number: "040,123456" },
-  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-  { id: 4, name: "Mary Poppendick", number: "39-23-6423122" },
-];
+app.use(cors());
+app.use(express.static("build"));
 
 // Definimos las rutas
 app.get("/ruta", (req, res) => {
-  res.send("<h2>Welcome to Kitoo piza...</h2>");
+  res.send("<h2>Welcome to Kitoo pizza...</h2>");
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((person) => {
+    res.json(person);
+  });
 });
 
 app.get("/info", (req, res) => {
-  const date = new Date();
-  const infoPersons = persons.length;
-  res.send(
-    `<div>Phonebook has info for ${infoPersons} people \n ${date}</div>`
-  );
+  Person.countDocuments({}, (err, count) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      const date = new Date();
+      res.send(
+        `<div>Phonebook has info for ${count} people \n ${date}</div>`
+      );
+    }
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const foundPerson = persons.find((person) => person.id === id);
-  if (!foundPerson) {
-    res.status(404).end();
-  }
-  res.json(foundPerson);
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 // Utilizamos morgan como middleware
-
-app.use(morgan("tiny")); //tiny , mantiene un registro red
-
-
+app.use(morgan("tiny"));
 app.use(
   morgan((tokens, req, res) => {
     if (req.method === "POST") {
@@ -53,49 +48,45 @@ app.use(
   })
 );
 
-// METODOS
+app.use(express.json());
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({ error: "malformatted id" });
+    });
 });
 
-app.use(express.json()); //Es para definir la propiedad body
-
-function generateId() {
-  let maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
-}
-
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "number or name missing",
+    return response.status(400).json({
+      error: "name or number is missing",
     });
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  res.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).json({ error: error.message });
+    });
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
